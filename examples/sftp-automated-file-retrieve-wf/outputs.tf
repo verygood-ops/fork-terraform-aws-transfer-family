9706764ea63b057e8da02a2d065e75c2e84cc2d5
nizar-lahlali <nizar.lahlali.1@ens.etsmtl.ca>
2025-07-22 14:29:39 -0400

    add outputs.tf and header.md

---
+++ examples/sftp-automated-file-retrieve-wf/outputs.tf
@@ -0,0 +1,29 @@
+output "server_id" {
+  description = "The ID of the created Transfer Family server"
+  value       = module.transfer_server.server_id
+}
+
+output "server_endpoint" {
+  description = "The endpoint of the created Transfer Family server"
+  value       = module.transfer_server.server_endpoint
+}
+
+output "sftp_bucket_name" {
+  description = "The name of the S3 bucket used for SFTP storage"
+  value       = module.s3_bucket.s3_bucket_id
+}
+
+output "document_storage_bucket_name" {
+  description = "The name of the S3 bucket used for document storage"
+  value       = module.document_storage_bucket.s3_bucket_id
+}
+
+output "document_storage_bucket_arn" {
+  description = "The ARN of the S3 bucket used for document storage"
+  value       = module.document_storage_bucket.s3_bucket_arn
+}
+
+output "user_details" {
+  description = "Map of users with their details including secret names and ARNs"
+  value = module.sftp_users.user_details
+}
