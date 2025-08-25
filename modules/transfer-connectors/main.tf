0fbd9a72baebe7292704afe339476938e85a08cf
nizar-lahlali <nizar.lahlali.1@ens.etsmtl.ca>
2025-08-24 22:15:44 -0400

    password rotation for

--- modules/transfer-connectors/main.tf
+++ modules/transfer-connectors/main.tf
@@ -47,6 +47,60 @@ resource "aws_secretsmanager_secret" "sftp_credentials" {
   kms_key_id  = var.secret_kms_key_id
 }
 
+resource "aws_secretsmanager_secret_rotation" "sftp_credentials_rotation" {
+  count           = var.create_secret ? 1 : 0
+  secret_id       = aws_secretsmanager_secret.sftp_credentials[0].id
+  rotation_lambda_arn = aws_lambda_function.rotation_lambda[0].arn
+
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
+
+resource "aws_lambda_function" "rotation_lambda" {
+  count         = var.create_secret ? 1 : 0
+  filename      = data.archive_file.rotation_lambda_zip[0].output_path
+  function_name = "secretsmanager-rotation-${var.secret_name}"
+  role          = aws_iam_role.rotation_lambda_role[0].arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+  timeout       = 30
+}
+
+resource "aws_iam_role" "rotation_lambda_role" {
+  count = var.create_secret ? 1 : 0
+  name  = "secretsmanager-rotation-role-${var.secret_name}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rotation_lambda_basic" {
+  count      = var.create_secret ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.rotation_lambda_role[0].name
+}
+
+data "archive_file" "rotation_lambda_zip" {
+  count       = var.create_secret ? 1 : 0
+  type        = "zip"
+  output_path = "/tmp/rotation_lambda.zip"
+  source {
+    content = "def lambda_handler(event, context): pass"
+    filename = "lambda_function.py"
+  }
+}
+
 resource "aws_secretsmanager_secret_version" "sftp_credentials" {
   count     = var.create_secret ? 1 : 0
   secret_id = aws_secretsmanager_secret.sftp_credentials[0].id
